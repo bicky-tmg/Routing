293a923dcda45fb8a40e06f1fed41f657af12a9f
Bicky Tamang <bickytamang101@gmail.com>
2022-05-23 10:47:58 +0545

    Building Nested Dynamic Routes & Paths

---
+++ pages/clients/index.js
@@ -0,0 +1,9 @@
+function ClientsPage() {
+  return (
+    <div>
+      <h1>The Client Page</h1>
+    </div>
+  );
+}
+
+export default ClientsPage;
--- pages/portfolio/[projectId].js
+++ pages/portfolio/[projectId].js
@@ -5,6 +5,10 @@ function PortfolioProjectPage() {
 
   console.log(router.pathname);
   console.log(router.query);
+
+  // send a request to some backend server
+  // to fetch the piece of data within an id of router.query.projectId
+  
   return (
     <div>
       <h1>The Portfolio Project Page</h1>
