fdea8314c04a91ca9ea6a9a83cc3cf2181f3ceb9
Bicky Tamang <bickytamang101@gmail.com>
2022-05-23 09:46:30 +0545

    Extracting Dynamic Path Segment Data

--- pages/portfolio/[projectId].js
+++ pages/portfolio/[projectId].js
@@ -1,4 +1,10 @@
+import { useRouter } from "next/router";
+
 function PortfolioProjectPage() {
+  const router = useRouter();
+
+  console.log(router.pathname);
+  console.log(router.query);
   return (
     <div>
       <h1>The Portfolio Project Page</h1>
